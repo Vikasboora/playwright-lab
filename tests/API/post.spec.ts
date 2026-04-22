47c8f9a0da5a823ad22a3b25bc38d95a6903c0e8
Vikas Kumar <vikaskumar077777@gmail.com>
2026-04-22 21:35:00 +0530

    added new post request

--- tests/API/post.spec.ts
+++ tests/API/post.spec.ts
@@ -19,3 +19,24 @@ test('Post API method Handling using Playwright request', async ({ request }) =>
   const responseData = await response.json();
   console.log(responseData);
 });
+
+test('Using Playwright request for post request', async ({ request }): Promise<void> => {
+  const response: APIResponse = await request.post('https://api.restful-api.dev/register', {
+    headers: {
+      'x-api-key': process.env.X_API_KEY!,
+      'Content-Type': 'application/json',
+    },
+    params: {
+      'expires-in': 360,
+      status: 200,
+    },
+    data: {
+      email: 'antonio@example.com',
+      password: 'securePassword123',
+      name: 'Antonio',
+    },
+  });
+
+  const responseData = await response.json();
+  console.log(responseData);
+});
